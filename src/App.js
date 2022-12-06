6b6fb67b4786517a6cc6a1fc82a578b8bbbb152b
lemerveille <mvaneck@methys.com>
2022-12-06 21:31:12 +0200

    added a basic router to be replaced with react router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import { Login } from './Login';
 import './App.css';
 import Register from './Register';
 import React, {useState} from 'react';
-function App() {
 
-  const [route, setRoute]= useState("login")
+// route === "login" && <Login setRoute={setRoute} />
+// if route === "login" then return <Login />
+// if route !== "login" then return fasle
+
+// a && b -> if a is true then return b
+// if a is false then return a (false)
+
+// in react we can use any falsy value to render nothing
+
+const routes = [
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+]
+
+function App() {
 
   return (
     <div className="App">
-
       {
-    
+        routes.map(route => (
+          window.location.href.includes(route.path) ? route.element : null
+        ))
       }
-      <Login></Login>
-   
     </div>
   );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,11 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
-export const Login= ()=>{
+export const Login = (props) => {
 
-    const [email, setEmail]=useState("");
-    const [password, setPassword]= useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleSubmit= e => {     
+    const handleSubmit = e => {     
         console.log(JSON.stringify(email, null, 2));
         e.preventDefault();
     }
@@ -14,13 +14,20 @@ export const Login= ()=>{
         <div>
            <form className="form" onSubmit={handleSubmit}>
             <label> Email</label>
-            <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" id="email" placeholder="yourname@gmail.com" name="email"></input>
+            <input
+                value={email}
+                onChange={(e)=>{setEmail(e.target.value)}}
+                type="email"
+                id="email"
+                placeholder="yourname@gmail.com"
+                name="email"
+            />
             <br/>
             <label>Password</label>
             <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" id="password" placeholder="Password" name="password"></input>
             <button type="onSubmit">Login</button>
            </form>
-           <button>Register here</button>
+           <button onClick={() => window.location.href = 'register'}>Register here</button>
         </div>
     )
 }; 
