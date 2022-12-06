be3163d1e8c7ee58ca4ec69f0ab34984f2f453e4
Malone99 <malonechawora@gmail.com>
2022-12-06 15:33:46 +0200

    commit#2

---
+++ src/Login.jsx
@@ -0,0 +1,26 @@
+import React,{ useState } from "react";
+
+export const Login= ()=>{
+
+    const [email, setEmail]=useState("");
+    const [password, setPassword]= useState("");
+
+    const handleSubmit= e => {     
+        console.log(JSON.stringify(email, null, 2));
+        e.preventDefault();
+    }
+
+    return(
+        <div>
+           <form className="form" onSubmit={handleSubmit}>
+            <label> Email</label>
+            <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" id="email" placeholder="yourname@gmail.com" name="email"></input>
+            <br/>
+            <label>Password</label>
+            <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" id="password" placeholder="Password" name="password"></input>
+            <button type="onSubmit">Login</button>
+           </form>
+           <button>Register here</button>
+        </div>
+    )
+}; 
