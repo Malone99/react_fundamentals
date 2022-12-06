d78d1ac8f2718758806db3cab7a38391fd8a6c1e
Malone99 <malonechawora@gmail.com>
2022-12-06 15:32:39 +0200

    commit#1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Login } from './Login';
 import './App.css';
-
+import Register from './Register';
+import React, {useState} from 'react';
 function App() {
+
+  const [route, setRoute]= useState("login")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {
+    
+      }
+      <Login></Login>
+   
     </div>
   );
 }
