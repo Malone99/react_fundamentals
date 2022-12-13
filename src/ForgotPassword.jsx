a6f9b9c9cb02a28f854024e42d4a718c5fa73fbb
Merveille van Eck <merveillevaneck@gmail.com>
2022-12-13 21:13:06 +0200

    added an example of useNavigate to forgot password

--- src/ForgotPassword.jsx
+++ src/ForgotPassword.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ForgotPasword = () => {
 
-    function handleChange() {
-    }
+    const navigate = useNavigate();
 
     return (
         <div>
             <h1>Forgot Password</h1>
             <Link to={'/reset-password'}>Reset Password</Link>
+            <button onClick={() => navigate('/reset-password')}>this does the same thing</button>
         </div>
     )
 
